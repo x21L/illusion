6b019c40b5fed5f90731f74df43887e4a926d352
Lukas Wais <lukas.wais@outlook.de>
2022-02-26 11:35:31 +0100

    Added license key

--- application/lib/config.dart
+++ application/lib/config.dart
@@ -1,7 +1,8 @@
 import 'package:camera_deep_ar/camera_deep_ar.dart';
 
 DeepArConfig config = const DeepArConfig(
-  androidKey: "",
+  androidKey:
+      "1c290c17a713fdc1100bca30d5b85fc092eea967a9a5e79502fe04500787e638b902fbf646d513c7",
   ioskey: "",
   displayMode: DisplayMode.camera,
 );
